2381c0c3f4360baca07a5cb877f7a888f8061472
Parth <parth0072@live.com>
2020-07-24 11:53:27 +0530

    - Added Package Swift

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MFCard",
+    products: [
+        platforms: [
+            .iOS(.v10),
+        ],
+        .library(
+        name: "MFCard",
+        targets: ["MFCard"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "MFCard",
+            dependencies: []),
+        .testTarget(
+            name: "MFCardTests",
+            dependencies: ["MFCard"]),
+    ]
+)
